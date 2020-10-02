15bb399a8deed0a17bfb7cad30446b30214e82c1
fabricio <fabricio.jacob@outlook.com>
2020-10-02 17:57:42 -0300

    add

--- customer/src/main/java/com/github/fabriciolfj/customer/CustomerApplication.java
+++ customer/src/main/java/com/github/fabriciolfj/customer/CustomerApplication.java
@@ -3,8 +3,12 @@ package com.github.fabriciolfj.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.github.fabriciolfj.customer.domain.repository")
+@EntityScan(basePackages = "com.github.fabriciolfj.customer.domain.entity")
 @EnableAutoConfiguration
 public class CustomerApplication {
 
