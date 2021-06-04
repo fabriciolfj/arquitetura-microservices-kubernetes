0fb046535e75a35bec9439cc1a95388b56702964
fabricio <fabricio.jacob@outlook.com>
2021-06-04 10:43:47 -0300

    add

--- inventario/src/test/java/com/github/fabriciolfj/inventario/InventarioApplicationTests.java
+++ inventario/src/test/java/com/github/fabriciolfj/inventario/InventarioApplicationTests.java
@@ -3,10 +3,10 @@ package com.github.fabriciolfj.inventario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InventarioApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
